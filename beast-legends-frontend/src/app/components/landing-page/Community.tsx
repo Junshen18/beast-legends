4cc100dfbd713cb5e4333c7048ea7768603d58a5
Junshen18 <junshenhere@gmail.com>
2025-04-01 03:21:50 +0800

    add link

--- beast-legends-frontend/src/app/components/landing-page/Community.tsx
+++ beast-legends-frontend/src/app/components/landing-page/Community.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { Reveal } from "../reveal";
 import Image from "next/image";
 import data from "../../data/landing-page.json";
+import { useRouter } from "next/navigation";
 
 export default function Community() {
+  const router = useRouter();
+
   return (
     <section className="bg-[url('/landing-page/community.png')] bg-cover bg-center pt-20 px-6" id="community">
       <div className="mx-auto flex flex-col gap-6 justify-center items-center relative">
@@ -28,7 +33,9 @@ export default function Community() {
             />
           </div>
         </Reveal>
-        <div className="text-xl md:text-2xl text-white border border-white px-6 md:px-10 py-2 rounded-full cursor-pointer hover:bg-white hover:text-black transition-all duration-300">
+        <div 
+          onClick={() => router.push("/communities")}
+          className="text-xl md:text-2xl text-white border border-white px-6 md:px-10 py-2 rounded-full cursor-pointer hover:bg-white hover:text-black transition-all duration-300">
           {data.community.buttonText}
         </div>
         <div className="w-full h-fit flex relative">
